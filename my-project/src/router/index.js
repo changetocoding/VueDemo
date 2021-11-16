a636097bff31fc765d17b8bcc7982aafe2d76c73
davmarksman <david.ike.mark@gmail.com>
2021-11-16 13:53:02 +0000

    Template for task 1

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import ProductDetails from '../views/ProductDetails.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,13 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/test',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/productDetails',
+    component: ProductDetails
   }
 ]
 
---
+++ my-project/src/views/ProductDetails.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Product Details Page</h1>
+
+    <div>
+      <div class="card sm-4 productCard mx-auto d-flex">
+        <a href="/" class="stretched-link">
+          <img class="card-img-top" src="../assets/OutdoorBonsai_1.jpg" />
+        </a>
+        <div class="img-centered-text">More Info</div>
+        <div class="card-body">
+          <p class="card-text">Chinese Juniper Itagawa<br />£2000.00</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          productTitle: 'Chinese Juniper Itagawa',
+          productPrice: 2000,
+          productDescription:'Spectacular Itoigawa Chinese Juniper Tree, approximately 35-39cm tall and in a ceramic bonsai pot.',
+          productImage: 'OutdoorBonsai_1.jpg',
+          checkoutOptions: ['Tools', 'Soil', 'Care'],
+        },
+        {
+          id: 2,
+          productTitle: 'Large Chinese Juniper Itagawa',
+          productPrice: 1495,
+          productDescription: 'Lovely Large Itoigawa Chinese Juniper Tree, approximately 98-100cm tall.',
+          productImage: 'OutdoorBonsai_2.jpg',
+          checkoutOptions: ['Tools', 'Soil', 'Care'],
+        },
+        {
+          id: 3,
+          productTitle: '35-38cm Itagawa',
+          productPrice: 3250,
+          productDescription: 'Beautiful Chinese Itiogawa Juniper, approximately 35-38cm tall and planted in an oval ceramic bonsai pot.',
+          productImage: 'OutdoorBonsai_3.jpg',
+          checkoutOptions: ['Tools', 'Soil', 'Care'],
+        },
+        {
+          id: 4,
+          productTitle: 'Brussels Bonsai',
+          productPrice: 195,
+          productDescription: 'Lovely Brussels Bonsai Tree in a ceramic bonsai pot, approximately 27-29cm tall.',
+          productImage: 'OutdoorBonsai_4.jpg',
+          checkoutOptions: ['Tools', 'Care'],
+        },
+        {
+          id: 5,
+          productTitle: 'Satsuki Azalea ',
+          productPrice: 595,
+          productDescription: 'Gorgeous Satsuki Azalea  Bonsai Tree, approximately 28-33cm tall and planted in an unglazed oval ceramic bonsai pot.',
+          productImage: 'OutdoorBonsai_5.jpg',
+          checkoutOptions: ['Tools', 'Care'],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
